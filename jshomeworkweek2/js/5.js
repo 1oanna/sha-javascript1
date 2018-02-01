1885220c85b8f57977238e961d39a9be0ea32a0c
1oanna <34710114+1oanna@users.noreply.github.com>
2018-02-01 02:23:56 +0200

    Add files via upload

---
+++ jshomeworkweek2/js/5.js
@@ -0,0 +1,13 @@
+/* Create a function called vehicle, like before, but takes another parameter called age, so that
+vehicle("blue", 1, 2) prints "a blue used car".
+vehicle("blue", 1, 1) prints "a blue new car". */
+
+
+function vehicle(color, age, type,) {
+ console.log(" a " +   color   +  age  +  type);
+}
+ 
+vehicle(" blue " , " used " , " car ");
+vehicle(" blue " , " new " , " car ");
+vehicle(" blue " , " used " , " motorbike ");
+vehicle(" blue " , " new " , " motorbike ");
---
+++ jshomeworkweek2/js/6.js
@@ -0,0 +1,4 @@
+// Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more.
+
+let vehicles = ["motorbike" , "caravan" , "bike" , "citycar" , "4x4"];
+console.log(vehicles);
