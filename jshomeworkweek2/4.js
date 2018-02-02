0eee9e761905f38a3ab79459cfafea5d2533f7c4
1oanna <34710114+1oanna@users.noreply.github.com>
2018-02-02 19:17:21 +0200

    Add files via upload

---
+++ jshomeworkweek2/4.js
@@ -0,0 +1,8 @@
+/*Can you write the following without the if statement, but with just as a single line with console.log(...);?
+if (3 == 3) {
+    console.log("true")
+} else {
+    console.log("false")
+}*/
+
+console.log(3==3);
---
+++ jshomeworkweek2/7.js
@@ -0,0 +1,6 @@
+/* How do you get the third element from vehicles?
+let vehicles = ["motorbike" , "caravan" , "bike" , "citycar" , "4x4"];
+console.log(vehicles); */
+
+vehicles[2]    //bike
+
