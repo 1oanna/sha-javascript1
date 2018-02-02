00b21b0694f41e29936cabcf8e79e71384613af8
1oanna <34710114+1oanna@users.noreply.github.com>
2018-02-02 19:23:23 +0200

    Add files via upload

---
+++ jshomeworkweek2/3.js
@@ -0,0 +1,42 @@
+/* Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike. 
+And prints "a blue motorbike" for example when called as vehicleType("blue", 2) */
+
+
+
+function vehicleType( color, type ) {
+	let strType;
+	if (type == 1)
+		strType = "car";
+
+	if (type == 2)
+		strType = "motorbike";
+
+	return "a " + color + " " + strType;
+}
+console.log(vehicleType("blue", 2));
+            
+vehicleType(" blue " , 1);
+vehicleType(" blue " , 2);
+// output "a  blue  motorbike"
+
+/*---- another way-----
+function vehicleType( color, type ) {
+	let strType;
+	if (type == "1")
+		strType = "car";
+
+	if (type == "2")
+		strType = "motorbike";
+
+	return "a " + color + " " + strType;
+
+}
+console.log(vehicleType("blue", 2));
+
+vehicleType(" blue " , "1");
+vehicleType(" blue " , "2");
+
+ //output "a  blue  motorbike" */
+
+
+
